e38c4807ac8d623f2f561f3b2c97cbf49e199498
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-06 15:25:46 -0500

    user can login

--- app/src/main/java/com/ethanjhowell/parstagram/LoginActivity.java
+++ app/src/main/java/com/ethanjhowell/parstagram/LoginActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getCanonicalName();
@@ -26,6 +30,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.d(TAG, "loginUser: Preparing to log in " + username);
-
+        ParseUser.logInInBackground(username, password, (ParseUser user, ParseException e) -> {
+            if (e != null) {
+                Log.e(TAG, "loginUser: login issue", e);
+                Toast.makeText(LoginActivity.this, "Unable to login", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.i(TAG, "loginUser: success for user " + username);
+                startActivity(MainActivity.getStartIntent(this));
+            }
+        });
     }
 }
