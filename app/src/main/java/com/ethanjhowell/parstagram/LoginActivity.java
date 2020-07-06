9f3dc55250de78b87d75b7f2ff6941f316771c8e
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-06 15:05:01 -0500

    created login screen

--- app/src/main/java/com/ethanjhowell/parstagram/LoginActivity.java
+++ app/src/main/java/com/ethanjhowell/parstagram/LoginActivity.java
@@ -1,14 +1,31 @@
 package com.ethanjhowell.parstagram;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getCanonicalName();
+    private EditText etUsername;
+    private EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        Button btLogin = findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(v -> loginUser(etUsername.getText().toString(), etPassword.getText().toString()));
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.d(TAG, "loginUser: Preparing to log in " + username);
+
     }
 }
