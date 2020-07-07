c68e39761d6e36607719dcb25850a70770c5f77d
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-07 16:25:21 -0500

    got the various fragments working swapping in on the bottom nav

--- app/src/main/java/com/ethanjhowell/parstagram/MainActivity.java
+++ app/src/main/java/com/ethanjhowell/parstagram/MainActivity.java
@@ -3,10 +3,14 @@ package com.ethanjhowell.parstagram;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.ethanjhowell.parstagram.fragments.CreateFragment;
+import com.ethanjhowell.parstagram.fragments.HomeFragment;
+import com.ethanjhowell.parstagram.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,22 +27,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        CreateFragment createFragment = new CreateFragment();
+        ProfileFragment profileFragment = new ProfileFragment();
+        HomeFragment homeFragment = new HomeFragment();
+
         BottomNavigationView bottom_navigation = findViewById(R.id.bottom_navigation);
-        bottom_navigation.setOnNavigationItemSelectedListener(this::bottomNavigationItemSelect);
+        bottom_navigation.setOnNavigationItemSelectedListener(menuItem -> {
+            Fragment fragment;
+            switch (menuItem.getItemId()) {
+                case R.id.action_create:
+                    fragment = createFragment;
+                    break;
+                case R.id.action_profile:
+                    fragment = profileFragment;
+                    break;
+                case R.id.action_home:
+                default:
+                    fragment = homeFragment;
+                    break;
+            }
+            fragmentManager.beginTransaction().replace(R.id.clFragment, fragment).commit();
+            return true;
+        });
+        bottom_navigation.setSelectedItemId(R.id.action_home);
     }
 
-
-    private boolean bottomNavigationItemSelect(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.action_home:
-                break;
-            case R.id.action_create:
-                break;
-            case R.id.action_profile:
-                break;
-        }
-        return true;
-    }
 
 
 }
