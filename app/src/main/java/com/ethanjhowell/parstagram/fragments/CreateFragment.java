308ec848e78b92a8d4b83a8851a599eaaf7fa162
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-07 16:08:21 -0500

    pulled non-activity stuff out of main activity, fragment seems to be working

--- app/src/main/java/com/ethanjhowell/parstagram/fragments/CreateFragment.java
+++ app/src/main/java/com/ethanjhowell/parstagram/fragments/CreateFragment.java
@@ -1,59 +1,50 @@
 package com.ethanjhowell.parstagram.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import com.ethanjhowell.parstagram.Post;
 import com.ethanjhowell.parstagram.R;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CreateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
+
 public class CreateFragment extends Fragment {
+    private static final String TAG = CreateFragment.class.getCanonicalName();
+    private final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1;
+    private static final String PHOTO_FILE_NAME = "photo.jpg";
+    private ParseQuery<Post> query;
+    private ImageView ivImagePreview;
+    private EditText etCaption;
+    private File photoFile;
+    private Context context;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public CreateFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreateFragment newInstance(String param1, String param2) {
-        CreateFragment fragment = new CreateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -61,5 +52,99 @@ public class CreateFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = view.getContext();
+
+        ivImagePreview = view.findViewById(R.id.ivImagePreview);
+        etCaption = view.findViewById(R.id.etCaption);
+
+        Button btTakePicture = view.findViewById(R.id.btTakePicture);
+        Button btSubmit = view.findViewById(R.id.btSubmit);
+
+        btSubmit.setOnClickListener(this::savePost);
+        btTakePicture.setOnClickListener(this::launchCamera);
+
+        query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        queryPosts();
+    }
+
+    private void savePost(View v) {
+        Post post = new Post(etCaption.getText().toString(), new ParseFile(photoFile), ParseUser.getCurrentUser());
+        post.saveInBackground(e -> {
+            if (e != null) {
+                Log.e(TAG, "savePost: Error saving post", e);
+                Toast.makeText(context, "Error saving post", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d(TAG, "savePost: Post save success!");
+                Toast.makeText(context, "Post save success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void launchCamera(View v) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri();
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(context, "com.ethanjhowell.parstagram.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap rawTakenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // Load the taken image into a preview
+                ivImagePreview.setImageBitmap(rawTakenImage);
+            } else { // Result was a failure
+                Toast.makeText(context, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private File getPhotoFileUri() {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "getPhotoFileUri: failed to create directory");
+        }
+
+        return new File(mediaStorageDir.getPath() + File.separator + PHOTO_FILE_NAME);
+    }
+
+    private void queryPosts() {
+        query.findInBackground((posts, e) -> {
+            if (e != null) {
+                Log.e(TAG, "queryPosts: problem getting posts", e);
+                Toast.makeText(context, "Something went wrong fetching posts", Toast.LENGTH_SHORT).show();
+            } else {
+                for (Post p : posts) {
+                    Log.i(TAG, String.format("queryPosts: Username: %s, Caption: %s", p.getUser().getUsername(), p.getCaption()));
+                }
+            }
+        });
     }
 }
