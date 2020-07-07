ea82f42f5638b17b293c4de881caaa49312aee90
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-07 11:14:35 -0500

    added parse query logic

--- app/src/main/java/com/ethanjhowell/parstagram/MainActivity.java
+++ app/src/main/java/com/ethanjhowell/parstagram/MainActivity.java
@@ -3,10 +3,15 @@ package com.ethanjhowell.parstagram;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseQuery;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getCanonicalName();
 
     public static Intent getStartIntent(Context context) {
         return new Intent(context, MainActivity.class);
@@ -17,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery.getQuery(Post.class).findInBackground((posts, e) -> {
+            if (e != null) {
+                Log.e(TAG, "queryPosts: problem getting posts", e);
+                Toast.makeText(this, "Something went wrong fetching posts", Toast.LENGTH_SHORT).show();
+            } else {
+                for (Post p : posts) {
+                    Log.i(TAG, "queryPosts: got post: " + p.getCaption());
+                }
+            }
+        });
     }
 }
