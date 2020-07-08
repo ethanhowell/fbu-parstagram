4d638d18ea60e64699f4cbc3113538a0bb51e43b
Ethan J. Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-08 15:00:59 -0500

    User can log out (#5)
    * user can now logout
    
    * update readme

--- app/src/main/java/com/ethanjhowell/parstagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/ethanjhowell/parstagram/fragments/ProfileFragment.java
@@ -1,19 +1,24 @@
 package com.ethanjhowell.parstagram.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.ethanjhowell.parstagram.LoginActivity;
 import com.ethanjhowell.parstagram.R;
+import com.parse.ParseUser;
 
 
 public class ProfileFragment extends Fragment {
 
+    // Required empty public constructor
     public ProfileFragment() {
-        // Required empty public constructor
     }
 
 
@@ -21,6 +26,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        view.findViewById(R.id.btLogout).setOnClickListener(v -> {
+            ParseUser.logOut();
+            startActivity(new Intent(view.getContext(), LoginActivity.class));
+            getActivity().finish();
+        });
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
