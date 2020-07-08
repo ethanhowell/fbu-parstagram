013b16832de6d881c553fb21f778c5316c990831
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-08 15:19:00 -0500

    registration can now be done

--- app/src/main/java/com/ethanjhowell/parstagram/LoginActivity.java
+++ app/src/main/java/com/ethanjhowell/parstagram/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,9 +31,29 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         Button btLogin = findViewById(R.id.btLogin);
+        TextView tvSignUp = findViewById(R.id.tvSignUp);
 
         btLogin.setOnClickListener(v -> loginUser(etUsername.getText().toString(), etPassword.getText().toString()));
+        tvSignUp.setOnClickListener(v -> registerUser(etUsername.getText().toString(), etPassword.getText().toString()));
 
+    }
+
+    private void registerUser(String username, String password) {
+        Log.d(TAG, "registerUser: Preparing to register " + username);
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(e -> {
+            if (e != null) {
+                Log.e(TAG, "registerUser: registration issue", e);
+                Toast.makeText(LoginActivity.this, "Unable to create account", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.i(TAG, "registerUser: success for user " + username);
+                startActivity(MainActivity.createIntent(this));
+                finish();
+            }
+        });
     }
 
     private void loginUser(String username, String password) {
