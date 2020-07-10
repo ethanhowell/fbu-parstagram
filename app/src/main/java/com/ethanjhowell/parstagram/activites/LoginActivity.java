6c4679d95e1c80e3866a5c7a654474f4e94e8fc7
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-10 14:09:24 -0500

    got the login working

--- app/src/main/java/com/ethanjhowell/parstagram/activites/LoginActivity.java
+++ app/src/main/java/com/ethanjhowell/parstagram/activites/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.ethanjhowell.parstagram.activites;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -10,6 +11,11 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ethanjhowell.parstagram.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
@@ -17,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getCanonicalName();
     private EditText etUsername;
     private EditText etPassword;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,33 @@ public class LoginActivity extends AppCompatActivity {
         btLogin.setOnClickListener(v -> loginUser(etUsername.getText().toString(), etPassword.getText().toString()));
         tvSignUp.setOnClickListener(v -> registerUser(etUsername.getText().toString(), etPassword.getText().toString()));
 
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager loginManager = LoginManager.getInstance();
+        loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess: ");
+                for (String p : loginResult.getRecentlyGrantedPermissions()) {
+                    Log.d(TAG, "onSuccess: " + p);
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel: ");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError: ");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void registerUser(String username, String password) {
